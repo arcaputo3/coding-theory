c5f4c1322b30b8dc2bafcc85feeaf1d2f3a31cea
Albert Richard Caputo III <rcaputo3@outlook.com>
2019-05-07 19:08:45 -0400

    Move non-pdf files to **/code_and_data/

--- Character/curveODE.m
+++
@@ -1,4 +0,0 @@
-function dgdt = curveODE(t,g,K,Xt)
-K = interp1(Xt, K, t); % Interpolate the data set (Xt, K) at times t
-dgdt = [g(2); K*(g(2))]; % Evalute ODE at times t
-end
--- Character/gderiv.m
+++
@@ -1,7 +0,0 @@
-function dgdt = gderiv(t, g, Xt, X1, X2, r)
-X1 = interp1(Xt, X1, t); % Interpolate the data set (Xt, X1) at times t
-X2 = interp1(Xt, X2, t); % Interpolate the data set (Xt, X2) at times t
-R = interp1(Xt, r, t);
- 
-dgdt = [g(2); (1/2*(exp(X1) - exp(X2)) ./ R).*g(2)];
-end
